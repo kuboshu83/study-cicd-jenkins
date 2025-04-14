b34ca937ae11a8a929a21295c22c4b5e947ae1a9
kuboshu83 <dot.sohkubo@gmail.com>
2025-04-14 19:41:32 +0900

    .envにコメントがわりの初期値設定

--- script/init.sh
+++ script/init.sh
@@ -2,7 +2,7 @@
 
 if [ ! -e .env ]; then
 	touch .env
-	echo DB_USER= >> .env
-	echo DB_PASSWORD= >> .env
-	echo DB_NAME= >> .env
+	echo DB_USER="please set database user name" >> .env
+	echo DB_PASSWORD="please set database user password" >> .env
+	echo DB_NAME="please set redmine database name" >> .env
 fi
